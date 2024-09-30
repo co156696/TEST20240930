ab919d13ef8ec0a308ffc87337dd7ade9ca3f705
Oguzhan Cebeci <156648312+co156696@users.noreply.github.com>
2024-09-30 07:08:03 +0000

    ...

--- klausur/klausur20240930.js
+++ klausur/klausur20240930.js
@@ -9,14 +9,28 @@ console.log("Aufgabe 1")
 // Es soll das Volumen eines Würfels in Quadratzentimeter (cm3) ausgegeben werden. Arbeiten Sie mit einem Objekt und sprechenden Eigenschaften.
 // Geben Sie das Volumen in einem Antwortsatz auf der Konsole aus.
 
+class Wuerfel{
+     constructor(){
+          this.seitenlage 
+     }
+    }
 
+let wuerfel= new Wuerfel
 
+wuerfel.seitenlage = 6
+ 
+let volumen = wuerfel.seitenlage*wuerfel.seitenlage*wuerfel.seitenlage
+console.log('das volumen des wuerfels mit der seitenlage' + wuerfel.seitenlage+ 'beträgt' + volumen + 'cm^3')
 
 // 1b 
 // Wenn das Volumen 1000 oder mehr Quadratzentimeter beträgt soll eine Meldung auf der Konsole ausgegeben werden, die besagt,
 // dass das Volumen 1000 oder mehr Quadratzentimeter beträgt.
 
-
+if(volumen){
+    console.log(' das volumen des würfels beträgt 1000cm^3 oder mehr')
+}else{
+   console.log('') 
+} 
 
 
 
@@ -29,17 +43,24 @@ console.log("Aufgabe 2")
 // Sie werden beauftragt ein Schulverwaltungsprogramm für das BKB zu erstellen. Konkret sollen Sie sich in einem ersten Schritt
 // die Zeugnisse vornehmen. Deklarieren, Instanziieren und Initialisieren Sie ein Zeugnisobjekt, 
 // in dem Sie neben "Deutsch", "Mathe" und "Englisch" 5 weitere, geeignete Eigenschaften deklarieren. 
+ 
+
+
 
 // 2a)
 // Definieren Sie die class
-
-
-
 // 2b)
 // Deklarieren, Instanziieren und Initialisieren Sie ein Objekt von der Zeignisklasse.
 
-
-
+class zeignisklasse{
+      constructor()}
+         this.Betriebswirtschaftslehre
+         this.Volkswirtschaftslehre
+         this.Wirtschaftsinformatik
+         this.Speziellewirtschaft
+         this.Biologie
+    {
+}
 
 // 2c)
 // Geben Sie die Eigenschaftswerte Ihres Objekts auf der Konsole aus.
@@ -86,13 +107,28 @@ console.log("Aufgabe 3")
 // 3a)
 // Deklarieren, Instanziieren Sie ein Objekt mit allen genannten Eigenschaften. 
 
+class Zeitung{
+    constructor()}
+     this.Rabatt
+     this.Nettorechnungsbetrag
+     this.MwST
+     thisBruttorechnungsbetrag
+     this.Skonto
+     this.Zahlungsbetrag
+{
+}
 
-
-
+let zeitung = new Zeitung()
 // 3b
 // Initialisieren Sie Ihr Objekt mit den gegebenen Werten. 
 // Geben Sie die einen Anwortsatz auf der Konsole aus.
 
+zeitung.Rabatt= listenpreis*Rabatt-Prozentsatz/ 100%
+zeitung.Nettorechnungsbetrag = 370 EUR
+zeitung.MwST = Nettorechnungsbetrag*MwST-Prozentsatz/100%
+zeitung.Brutto = Nettorechnungsbetrag + MwST
+zeitung.Skonto = Bruttorechnungsbetrag* Skonto-Prozentsatz/ 100%
+zeitung.zahlungsbetrag = Bruttorechnungsbetrag - Skonto
 
 
 // 3c)
